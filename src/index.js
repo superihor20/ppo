18bd2454ce19e40af7c8b35ffc0ba3ee3546c641
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-28 14:12:18 +0300

    add directions

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const createImage = (src) => {
 };
 
 (() => {
+  const directions = document.querySelector('.directions');
   const acceptButton = document.querySelector('.accept-button');
   const dangerButtons = document.querySelectorAll('.danger-button');
 
@@ -37,4 +38,15 @@ const createImage = (src) => {
   dangerButtons.forEach((button) => {
     button.addEventListener('click', onDangerButtonClick);
   });
+
+  let gyroscope = new Gyroscope({ frequency: 60 });
+
+  gyroscope.addEventListener('reading', (e) => {
+    const res = `Angular velocity along the X-axis ${gyroscope.x}
+    Angular velocity along the Y-axis ${gyroscope.y}
+    Angular velocity along the Z-axis ${gyroscope.z}`;
+    directions.innerHTML = '';
+    directions.appendChild(res);
+  });
+  gyroscope.start();
 })();
