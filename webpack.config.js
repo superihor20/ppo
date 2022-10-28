04d7ea0c5bdaa1c9c7e9745e8579e2a17d4ca682
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-28 11:41:29 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: false },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'PPO',
+      template: path.join('src', 'assets', 'html', 'index.html'),
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+};
