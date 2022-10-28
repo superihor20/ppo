e87ff67c6db41beeb314b716e7d7d9cb320f8cac
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-28 14:31:40 +0300

    compass

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const createImage = (src) => {
 };
 
 (() => {
-  const directions = document.querySelector('.directions');
+  const directions = document.querySelector('.compass');
   const acceptButton = document.querySelector('.accept-button');
   const dangerButtons = document.querySelectorAll('.danger-button');
 
@@ -38,20 +38,32 @@ const createImage = (src) => {
   dangerButtons.forEach((button) => {
     button.addEventListener('click', onDangerButtonClick);
   });
-  DeviceOrientationEvent.requestPermission().then(() => {
-    let gyroscope = new Gyroscope({ frequency: 60 });
-    const start = document.createElement('span');
-    start.innerHTML = 'somess';
-    directions.appendChild(start);
-    gyroscope.addEventListener('reading', (e) => {
-      const s = document.createElement('span');
-      const res = `Angular velocity along the X-axis ${gyroscope.x}
-      Angular velocity along the Y-axis ${gyroscope.y}
-      Angular velocity along the Z-axis ${gyroscope.z}`;
-      s.innerHTML = res;
-      // directions.innerHTML = '';
-      directions.appendChild(s);
-    });
-    gyroscope.start();
-  });
+
+  const isIOS = !(
+    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+    navigator.userAgent.match(/AppleWebKit/)
+  );
+
+  let compass;
+
+  () => {
+    if (isIOS) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === 'granted') {
+            window.addEventListener('deviceorientation', handler, true);
+          } else {
+            alert('has to be allowed!');
+          }
+        })
+        .catch(() => alert('not supported'));
+    } else {
+      window.addEventListener('deviceorientationabsolute', handler, true);
+    }
+  };
+
+  function handler(e) {
+    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+    directions.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+  }
 })();
