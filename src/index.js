3ac11001c22fc6ac5dbf18f6117eb7a1bf7c384a
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-28 14:46:24 +0300

    copy paser

--- src/index.js
+++ src/index.js
@@ -39,38 +39,84 @@ const createImage = (src) => {
     button.addEventListener('click', onDangerButtonClick);
   });
 
-  const isIOS = !(
-    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-    navigator.userAgent.match(/AppleWebKit/)
-  );
-
-  console.log(isIOS);
-
-  let compass;
-
   (() => {
-    alert('started');
-    if (isIOS) {
-      alert('ios');
-      DeviceOrientationEvent.requestPermission()
-        .then((response) => {
-          if (response === 'granted') {
-            window.addEventListener('deviceorientation', handler, true);
-          } else {
-            alert('has to be allowed!');
-          }
-        })
-        .catch(() => alert('not supported'));
-    } else {
-      alert('not ios');
-      window.addEventListener('deviceorientationabsolute', handler, true);
-    }
-  })();
+    let compass;
+    const isIOS =
+      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+      navigator.userAgent.match(/AppleWebKit/);
 
-  function handler(e) {
-    alert('ya nadler');
-    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    alert(compass, 'compass');
-    directions.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
-  }
+    function init() {
+      startCompass();
+      navigator.geolocation.getCurrentPosition(locationHandler);
+
+      if (!isIOS) {
+        window.addEventListener('deviceorientationabsolute', handler, true);
+      }
+    }
+
+    function startCompass() {
+      if (isIOS) {
+        DeviceOrientationEvent.requestPermission()
+          .then((response) => {
+            if (response === 'granted') {
+              window.addEventListener('deviceorientation', handler, true);
+            } else {
+              alert('has to be allowed!');
+            }
+          })
+          .catch(() => alert('not supported'));
+      }
+    }
+
+    function handler(e) {
+      compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+      directions.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+
+      // ±15 degree
+      if (
+        (pointDegree < Math.abs(compass) &&
+          pointDegree + 15 > Math.abs(compass)) ||
+        pointDegree > Math.abs(compass + 15) ||
+        pointDegree < Math.abs(compass)
+      ) {
+        myPoint.style.opacity = 0;
+      } else if (pointDegree) {
+        myPoint.style.opacity = 1;
+      }
+    }
+
+    let pointDegree;
+
+    function locationHandler(position) {
+      const { latitude, longitude } = position.coords;
+      pointDegree = calcDegreeToPoint(latitude, longitude);
+
+      if (pointDegree < 0) {
+        pointDegree = pointDegree + 360;
+      }
+    }
+
+    function calcDegreeToPoint(latitude, longitude) {
+      // Qibla geolocation
+      const point = {
+        lat: 21.422487,
+        lng: 39.826206,
+      };
+
+      const phiK = (point.lat * Math.PI) / 180.0;
+      const lambdaK = (point.lng * Math.PI) / 180.0;
+      const phi = (latitude * Math.PI) / 180.0;
+      const lambda = (longitude * Math.PI) / 180.0;
+      const psi =
+        (180.0 / Math.PI) *
+        Math.atan2(
+          Math.sin(lambdaK - lambda),
+          Math.cos(phi) * Math.tan(phiK) -
+            Math.sin(phi) * Math.cos(lambdaK - lambda)
+        );
+      return Math.round(psi);
+    }
+
+    init();
+  })();
 })();
